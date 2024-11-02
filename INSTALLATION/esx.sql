1e8680897b639e43db7987b8ad100db7b41f9353
BScript <bscript90@gmail.com>
2024-11-02 08:02:38 +0300

    Create esx.sql

---
+++ INSTALLATION/esx.sql
@@ -0,0 +1,5 @@
+INSERT IGNORE INTO `items` (`name`, `label`, `weight`, `rare`, `can_remove`) VALUES
+	('whiskey', 'Whiskey', 1, 0, 1),
+	('beer', 'Beer', 1, 0, 1),
+	('vodka', 'Vodka', 1, 0, 1),
+	('alcoholmeter', 'Alcoholmeter', 1, 0, 1);
